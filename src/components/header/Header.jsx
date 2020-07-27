d0720edab7e40e23aa078204165948a3607b37b1
Rian Marlon <rianmarlon20@yahoo.com>
2020-07-27 19:01:07 -0300

    Adicionando o componente ButtonLink na pasta do componente Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Logo from '../../assets/img/rianflix.png';
 import './Header.css';
 
+import ButtonLink from './components/buttonLink/ButtonLink';
+
 export default function Header() {
     return (
         <header className="header">
             <a href="/">
                 <img className="logo" src={Logo} alt="Logo do RianFlix"/>
             </a>
+
+            <ButtonLink className="button-link" href="/">
+                Novo vídeo
+            </ButtonLink>
         </header>
     );
 }
