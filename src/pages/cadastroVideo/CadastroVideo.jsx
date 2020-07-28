3d993228f00cfed9c4c0561d071dc5f5dcf245f3
Rian Marlon <rianmarlon20@yahoo.com>
2020-07-28 13:13:57 -0300

    Modificando o componente CadastroVideo

--- src/pages/cadastroVideo/CadastroVideo.jsx
+++ src/pages/cadastroVideo/CadastroVideo.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CadastroVideo() {
     return (
         <div>
-            Página de Cadastro de vídeo
+            <h1>Página de Cadastro de vídeo</h1>
+
+            <Link to="/cadastro/categoria">
+                Cadastro categoria
+            </Link>
         </div>
     )
 }
