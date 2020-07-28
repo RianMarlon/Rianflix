9defb330f1f7ed79029cbdda7e97e7b9c492fa02
Rian Marlon <rianmarlon20@yahoo.com>
2020-07-28 13:33:33 -0300

    Modificando o CSS do index.css

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ const Main = styled.main`
     background-color: var(--main-background-color);
     color: var(--white);
     flex: 1;
-    padding-top: 50px;
     padding-left: 5%;
     padding-right: 5%;
+    padding-bottom: 50px;
 `
 
 export default function App() {
