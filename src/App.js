893fe9ecee79f18b6fa7b040f8117b00313094d8
Rian Marlon <rianmarlon20@yahoo.com>
2020-07-28 12:31:03 -0300

    Alterando o componente Home e o App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/Header';
 import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
 
 export default function App() {
     return (
-        <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/" component={Home} />
-        </Switch>
+        <Fragment>
+            < Header />
+            <main>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/" component={Home} />
+                </Switch>
+            </main>
+            <Footer />
+        </Fragment>
     );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,38 +1,30 @@
-import React from 'react';
-import Header from '../../components/header/Header';
+import React, { Fragment } from 'react';
 import BannerMain from '../../components/bannerMain/BannerMain';
 import Carousel from '../../components/carousel/Carousel';
-import Footer from '../../components/footer/Footer';
 
 import dadosIniciais from '../../data/dadosIniciais.json';
 
 export default function Home() {
     return (
-        <div>
-            <Header />
+        <Fragment>
+            <BannerMain
+                videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+                url={dadosIniciais.categorias[0].videos[0].url}
+                videoDescription={"Neste vídeo irei falar sobre como organizar os módulos e pacotes da sua aplicação. A ideia apresentada é focar mais no negócio (domínio) do que em organizações meramente tecnológicas."}
+            />
 
-            <main>
-                <BannerMain
-                    videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-                    url={dadosIniciais.categorias[0].videos[0].url}
-                    videoDescription={"Neste vídeo irei falar sobre como organizar os módulos e pacotes da sua aplicação. A ideia apresentada é focar mais no negócio (domínio) do que em organizações meramente tecnológicas."}
-                />
+            <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais.categorias[0]}
+            />
 
-                <Carousel
-                    ignoreFirstVideo
-                    category={dadosIniciais.categorias[0]}
-                />
+            <Carousel
+                category={dadosIniciais.categorias[1]}
+            />
 
-                <Carousel
-                    category={dadosIniciais.categorias[1]}
-                />
-
-                <Carousel
-                    category={dadosIniciais.categorias[2]}
-                />      
-            </main>
-
-            <Footer />
-        </ div>
+            <Carousel
+                category={dadosIniciais.categorias[2]}
+            />      
+        </ Fragment>
     );
 }
