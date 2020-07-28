8308054e93642c5a411d5d13bbe55f3e977c5280
Rian Marlon <rianmarlon20@yahoo.com>
2020-07-28 17:36:48 -0300

    Alterando a cor do botão de 'assistir'

--- src/components/bannerMain/styles.js
+++ src/components/bannerMain/styles.js
@@ -112,8 +112,8 @@ export const WatchButton = styled.button`
     border-radius: 5px;
     text-decoration: none;
     border: 1px solid transparent;
-    color: var(--black);
-    background: var(--white);
+    color: var(--white);
+    background-color: var(--primary);
     border-color: var(--black);
     transition: opacity .3s;
     display: none;
