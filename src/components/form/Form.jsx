8140a331d027dc519a99d8212d92572ef502d583
Rian Marlon <rianmarlon20@yahoo.com>
2020-07-30 00:57:41 -0300

    Adicionando o styled-component Form

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Form = styled.form`
+    margin-top: 20px;
+    margin-bottom: 35px;
+    display: flex;
+    flex-direction: column;
+`
+
+export default Form;
