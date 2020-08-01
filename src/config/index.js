d856406d3734be147b0e118f16c716cb04dfb785
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-01 00:29:36 -0300

    Adicionando a pasta config com o arquivo index.html contendo a variável com o link do servidor

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const hasLocalHost = window.location.hostname.includes('localhost');
+const URL_BACKEND = hasLocalHost
+  ? 'http://localhost:8080'
+  : 'https://rianflix.herokuapp.com';
+
+export default URL_BACKEND;
